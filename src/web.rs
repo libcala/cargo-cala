7df050b4afda3d125e78720d8a7de7fcd6c6cab9
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-12-20 12:35:24 -0600

    Build in webserver for deploying to web.

--- src/web.rs
+++ src/web.rs
@@ -1,6 +1,29 @@
 use super::run;
+use actix_web;
+use actix_files;
+
+use actix_web::{HttpServer, App as ActixApp, HttpRequest, HttpResponse, web};
+use actix_files as fs;
 
 pub(super) fn web() {
     println!("Building for Web…");
 
+    let mut ip_port = "0.0.0.0:8080";
+    println!("Running on {}…", ip_port);
+
+    let mut server = HttpServer::new(move ||
+        ActixApp::new()
+            .service(web::resource("/{page}").route(
+                web::get().to(|_req: HttpRequest, path: web::Path<(String,)>| {
+                    fs::NamedFile::open(format!("./res/web/{}", path.0))
+                }
+            )))
+            .default_service(
+                web::get().to(|_req: HttpRequest| {
+                    fs::NamedFile::open("./res/web/404.html")
+                })
+            )
+    );
+
+    server.bind(ip_port).unwrap().run().unwrap();
 }
