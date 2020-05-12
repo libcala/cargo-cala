a1164d71012bfbdeeab277786b313d9e91d6fa2a
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-05-12 18:44:11 -0500

    Implement "run" command for Linux (flatpak).

--- src/run/linux.rs
+++ src/run/linux.rs
@@ -2,6 +2,7 @@
 
 use std::path::Path;
 use std::cell::RefCell;
+use std::process::Command;
 use cargo::util::config::Config;
 use cargo::ops::CompileOptions;
 use cargo::core::compiler::BuildConfig;
@@ -17,6 +18,7 @@ use cargo::core::compiler::CompileKind;
 use cargo::core::compiler::MessageFormat;
 use cargo::core::Workspace;
 use cargo::core::shell::Verbosity;
+use cargo::core::manifest::EitherManifest;
 
 struct Executor;
 
@@ -46,8 +48,26 @@ pub(super) fn run() {
     let config = Config::default().unwrap();
     config.shell().set_verbosity(Verbosity::Normal);
 
+    // Load package information
+    let manifest = match cargo::util::toml::read_manifest(&path, cargo::core::SourceId::for_path(&std::env::current_dir().unwrap()).unwrap(), &config).unwrap().0 {
+        EitherManifest::Real(manifest) => manifest,
+        EitherManifest::Virtual(_) => panic!("Virtual manifest not supported!"),
+    };
+    let metadata = manifest.metadata();
+
+    let app_name = manifest.name().as_str();
+    let app_description = &metadata.description;
+    let app_name_en = app_name; // FIXME
+    let app_description_en = app_description; // FIXME
+//    let app_company = custom_metadata();
+    let app_company = "company"; // FIXME
+    let app_domain = format!("cala.{}.{}", app_company, app_name);
+
     // Create directory structure
     std::fs::create_dir_all("./target/cargo_cala/run/app/files/bin/").unwrap();
+    std::fs::create_dir_all("./target/cargo_cala/run/app/export/share/icons/hicolor/scalable/apps/").unwrap();
+    std::fs::create_dir_all("./target/cargo_cala/run/app/export/share/applications/").unwrap();
+    std::fs::create_dir_all("./target/cargo_cala/run/repo/").unwrap();
 
     // Invoke cargo
     cargo::ops::compile_with_exec(
@@ -78,6 +98,91 @@ pub(super) fn run() {
         },
         &executor.into()
     ).unwrap();
+    
+    println!("[cargo-cala] Building flatpak…");
 
-    println!("Building flatpak…");
+    // Create Flatpak Files
+    std::fs::write(
+        format!("./target/cargo_cala/run/app/metadata"),
+        format!(
+            "[Application]
+name={app_domain}
+runtime=org.gnome.Platform/x86_64/3.32
+command={app_name}
+
+[Context]
+shared=ipc;network;
+sockets=x11;wayland;pulseaudio;
+devices=dri;
+filesystems=host;",
+            app_name = app_name, app_domain = app_domain,
+        )
+    ).unwrap();
+    
+    let mut desktop: String = format!(
+        "[Desktop Entry]
+Type=Application
+Name={app_name}
+Exec={app_name}
+Icon={app_domain}
+Terminal=false
+
+Name[en]={app_name_en}",
+        app_name = app_name, app_name_en = app_name_en,
+        app_domain = app_domain,
+    );
+    if let Some(description_en) = app_description_en {
+        desktop.push_str(&format!("\nComment={app_description_en}
+Comment[en]={app_description_en}", app_description_en = description_en));
+    }
+    
+    std::fs::write(
+        format!("./target/cargo_cala/run/app/export/share/applications/{app_domain}.desktop", app_domain = app_domain), desktop
+    ).unwrap();
+
+    // Invoke flatpak build
+    Command::new("flatpak")
+        .arg("build-export")
+        .arg("target/cargo_cala/run/repo/")
+        .arg("target/cargo_cala/run/app/")
+        .spawn()
+        .expect("Failed to run flatpak")
+        .wait()
+        .expect("Failed to terminate flatpak");
+        
+    // Install locally using project-specific repo
+    println!("[cargo-cala] Installing…");
+    Command::new("flatpak")
+        .arg("--user")
+        .arg("remote-add")
+        .arg("--no-gpg-verify")
+        .arg("--if-not-exists")
+        .arg(&app_domain)
+        .arg("target/cargo_cala/run/repo/")
+        .spawn()
+        .expect("Failed to run flatpak")
+        .wait()
+        .expect("Failed to terminate flatpak");
+    Command::new("flatpak")
+        .arg("--user")
+        .arg("install")
+        .arg("--reinstall")
+        .arg("--noninteractive")
+        .arg(&app_domain)
+        .arg(&app_domain)
+        .arg("-y")
+        .spawn()
+        .expect("Failed to run flatpak")
+        .wait()
+        .expect("Failed to terminate flatpak");
+    
+    // Run program
+    println!("[cargo-cala] Running…");
+    Command::new("flatpak")
+        .arg("run")
+        .arg(&app_domain)
+        .spawn()
+        .expect("Failed to run flatpak")
+        .wait()
+        .expect("Failed to terminate flatpak");
 }
